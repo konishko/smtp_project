9b64ce82952c69a3f3a62f739863e27514eeac10
Voice1081 <voice1081@gmail.com>
2019-10-15 23:57:12 +0500

    login works

--- src/SMTP/SMTP.java
+++ src/SMTP/SMTP.java
@@ -1,56 +1,64 @@
 package SMTP;
 
-
-import com.sun.xml.internal.org.jvnet.staxex.Base64EncoderStream;
-import sun.misc.BASE64Encoder;
-
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Scanner;
 
 public class SMTP {
     private SSLSocket sock;
-    private BufferedReader br;
+    private Scanner sc;
     private BufferedOutputStream bos;
     private Base64.Encoder encoder;
 
-    private SMTP() throws Exception {
+    public SMTP(){
         encoder = Base64.getEncoder();
         SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory
                 .getDefault();
         try {
             sock = (SSLSocket) sslsocketfactory.createSocket(
-                    "smtp.yandex.ru", 587);
+                    "smtp.yandex.ru", 465);
         } catch (IOException e) {
             e.printStackTrace();
         }
         try {
-            br = new BufferedReader(new
+            sc = new Scanner(new
                     InputStreamReader(sock.getInputStream()));
+            sc.useDelimiter("\r\n");
             bos = new BufferedOutputStream(sock.getOutputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String data = ReceiveData();
-        if (data.split(" ")[0] != "220")
-            throw new Exception("Can't start tls or server doesn't support the secure connection");
+        try {
+            ReceiveData();
+            SendData("EHLO user".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        if (data.split(" ")[0] != "220")
+//            throw new Exception("Can't start tls or server doesn't support the secure connection");
     }
 
+    private String ReceiveData(){
+        StringBuilder sb = new StringBuilder();
+        String data;
+        while((data = sc.nextLine()).charAt(3) == '-') {
+            sb.append(data);
+            sb.append('\n');
+        }
+        sb.append(data);
+        sb.append('\n');
+        data = sb.toString();
+        System.out.println(data);
+        return data;
+    }
     private String SendData(byte[] data) throws IOException {
         bos.write(data);
         bos.write("\r\n".getBytes());
+        bos.flush();
         return ReceiveData();
-    }
-
-    private String ReceiveData() throws IOException {
-        StringBuilder sb = new StringBuilder();
-        String rdata;
-        while ((rdata = br.readLine()) != null) {
-            sb.append(rdata);
-        }
-        return sb.toString();
     }
     public boolean Login(String login, String password){
         String data = null;
